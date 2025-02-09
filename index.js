2c2b27eb17186767e2275ac5164971af721915bb
Brice gateau <gateau245@gmail.com>
2025-02-09 14:07:50 +0100

    modification du index.html

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /*--- const ---*/
 //const tel = document.querySelector(".tel");
-//const a = document.querySelector(".a");
+const a = document.querySelector(".a");
 
 /*--- function ---*/
 a.addEventListener("click", () => {
-  const id = document.getElementById("id-iden");
-  id.classList.toggle("iden-contacte");
+  //const id = document.getElementById("id-iden");
+  //id.classList.toggle("iden-contacte");
+  const body = document.body;
+  body.style.backgroundColor = "red";
 });
-
-const body = document.body;
-body.style.backgroundColor = "red";
