f7f24c22d9f72599f36126990311314839804f5e
Brice gateau <gateau245@gmail.com>
2025-02-09 13:26:58 +0100

    modification du index.html

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const ul = document.getElementById("ul");
 const tel = document.querySelector(".tel");
 const a = document.querySelector(".a");
+const id = document.querySelector("iden-contacte");
 
 /*--- function ---*/
-a.addEventListener("click", () => {
-  console.log("ok");
-});
+a.addEventListener("click", () => {});
