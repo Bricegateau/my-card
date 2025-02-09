00d246f5de803b57d07719e3b40372a218204726
Brice gateau <gateau245@gmail.com>
2025-02-09 13:21:06 +0100

    modification du index.html

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /*--- const ---*/
 const ul = document.getElementById("ul");
 const tel = document.querySelector(".tel");
-console.log(tel);
 const a = document.querySelector(".a");
 
 /*--- function ---*/
-ul.addEventListener("click", () => {
-  tel.classList.toggle(".tel");
+a.addEventListener("click", () => {
+  console.log("ok");
 });
